e6ece5fee68add05fea10616ac65581b8b46d623
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-10-04 23:35:17 -0700

    Adds role_id to QuestionPolicy

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -19,7 +19,7 @@ class QuestionPolicy
    */
   public function update(User $user, Question $question)
   {
-    return $user->id == $question->user_id;
+    return $user->id == $question->user_id || $user->role_id == 0;
   }
 
   /**
@@ -31,6 +31,6 @@ class QuestionPolicy
    */
   public function delete(User $user, Question $question)
   {
-    return $user->id == $question->user_id && $question->answers_count == 0;
+    return ($user->id == $question->user_id || $user->role_id == 0) && $question->answers_count == 0;
   }
 }
