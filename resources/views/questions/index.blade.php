a9f6e79b51163dfeb74e95889c9e1ee38821bcc7
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-10-04 18:59:25 -0700

    Build Question Form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,22 +5,28 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
-
+                <div class="card-header">
+                  <div class="d-flex align-items-center">
+                    <h2>All Questions</h2>
+                    <div class="ml-auto">
+                      <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                    </div>
+                  </div>
+                </div><!-- card-header -->
                 <div class="card-body">
                   @foreach($questions as $question)
                     <div class="media">
-                      <div class="d-flex flex-column counters">
+                      <div class="counters d-flex flex-column">
                         <div class="vote">
                           <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
-                        </div>
+                        </div><!-- vote -->
                         <div class="status {{ $question->status }}">
                           <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers) }}
-                        </div>
+                        </div><!-- status -->
                         <div class="view">
                           {{ $question->views }} {{ Str::plural('view', $question->views) }}
-                        </div>
-                      </div>
+                        </div><!-- view -->
+                      </div><!-- counters -->
                       <div class="media-body">
                         <h3 class="mt-0"><a href="{{ $question->url }}" >{{ $question->title }}</a></h3>
                         <p class="lead">
@@ -28,14 +34,16 @@
                           <small class="text-muted">{{ $question->created_date }}</small>
                         </p>
                         {{ Str::limit($question->body, 250) }}
-                      </div>
-                    </div>
+                      </div><!-- media-body -->
+                    </div><!-- media -->
                     <hr>
                   @endforeach
-                  <div class="mx-auto">{{ $questions->links() }}</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                  <div class="mx-auto">
+                    {{ $questions->links() }}
+                  </div><!-- mx-auto -->
+                </div><!-- card-body -->
+            </div><!-- card -->
+        </div><!-- col -->
+    </div><!-- row -->
+</div><!-- container -->
 @endsection
