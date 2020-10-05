1ceff1103e230cd345896d5a598b230a987e2b12
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-10-04 23:29:38 -0700

    Adds role_id to users and inserts two default users in Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Question;
+use App\Answer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,40 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
-    factory(User::class, 3)->create()->each(function($user) {
+    \DB::table('users')->insert([
+      [
+        'name' => 'Julio (Admin)',
+        'email' => 'julio_1796@hotmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('secretpassword'),
+        'role_id' => 0,
+        'remember_token' => Str::random(10),
+        'created_at' => now(),
+        'updated_at' => now()
+      ],
+      [
+        'name' => 'Julio (User)',
+        'email' => 'invitado@hotmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('secretpassword'),
+        'role_id' => 1,
+        'remember_token' => Str::random(10),
+        'created_at' => now(),
+        'updated_at' => now()
+      ]
+    ]);
+    factory(User::class, 5)->create();
+
+    foreach(User::all() as $user)
+    {
       $user->questions()->saveMany(
-        factory(Question::class, rand(1, 5))->make()
-      );
-    });
+        factory(Question::class, rand(2, 5))->make()
+      )
+      ->each(function($question) {
+        $question->answers()->saveMany(
+          factory(Answer::class, rand(2, 5))->make()
+        );
+      });
+    }
   }
 }
