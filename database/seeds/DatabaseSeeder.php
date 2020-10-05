cd52f8fdae26cdce84102262ec978a093d19c176
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-10-04 17:10:23 -0700

    Users and Questions tables Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,22 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Question;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    factory(User::class, 3)->create()->each(function($user) {
+      $user->questions()->saveMany(
+        factory(Question::class, rand(1, 5))->make()
+      );
+    });
+  }
 }
