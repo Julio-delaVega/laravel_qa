832385b3559fe6c71299aa4b7eb10b150adb32b9
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-10-20 22:18:01 -0700

    DRY Principle (continued)

--- resources/views/shared/_votes.blade.php
+++ resources/views/shared/_votes.blade.php
@@ -9,12 +9,18 @@
     $firstURISegment = 'answers';
   @endphp
 @endif
+
+@php
+  $form_id = $name.'-'.$model->id;
+  $form_action = "/$firstURISegment/$model->id/vote"
+@endphp
+
 <div class="d-flex flex-column vote-controls">
-  <form id="up-vote-{{ $name }}-{{ $model->id }}" action="/{{ $firstURISegment }}/{{ $model->id }}/vote" method="POST">
+  <form id="up-vote-{{ $form_id }}" action="/{{ $form_action }}/vote" method="POST">
     @csrf
     <input type="hidden" name="vote" value="1">
     <a title="This {{ $name }} is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-      onclick="event.preventDefault(); document.getElementById('up-vote-{{ $name }}-{{ $model->id }}').submit()"
+      onclick="event.preventDefault(); document.getElementById('up-vote-{{ $form_id }}').submit()"
     >
       <i class="fas fa-caret-up fa-3x"></i>
     </a>
@@ -22,11 +28,11 @@
   <span class="votes-count">
     {{ $model->votes_count }}
   </span>
-  <form id="down-vote-{{ $name }}-{{ $model->id }}" action="/{{ $firstURISegment }}/{{ $model->id }}/vote" method="POST">
+  <form id="down-vote-{{ $form_id }}" action="/{{ $form_action }}/vote" method="POST">
     @csrf
     <input type="hidden" name="vote" value="-1">
     <a title="This {{ $name }} is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-      onclick="event.preventDefault(); document.getElementById('down-vote-{{ $name }}-{{ $model->id }}').submit()"
+      onclick="event.preventDefault(); document.getElementById('down-vote-{{ $form_id }}').submit()"
     >
       <i class="fas fa-caret-down fa-3x"></i>
     </a>
