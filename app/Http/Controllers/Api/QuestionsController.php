2cfe24560abe0087adc6d10c96d79053f81a9086
Julio Angel Sanchez de la Vega Buenrostro <desarrollo@arkanari.com>
2020-11-04 21:34:27 -0800

    Create endpoint to create questions

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Question;
 use Illuminate\Http\Request;
 use App\Http\Resources\QuestionResource;
+use App\Http\Requests\CreateQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -26,9 +27,14 @@ class QuestionsController extends Controller
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
-  public function store(Request $request)
+  public function store(CreateQuestionRequest $request)
   {
-    //
+    $question = $request->user()->questions()->create($request->all());
+
+    return response()->json([
+      'message' => 'Your question has been submitted.',
+      'question' => new QuestionResource($question)
+    ]);
   }
 
   /**
